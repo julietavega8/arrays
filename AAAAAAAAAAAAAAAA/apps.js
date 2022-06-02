e7c9297e3d18a1e9462f630b3966f208f4a668cc
julietavega8 <85358049+julietavega8@users.noreply.github.com>
2022-06-02 09:06:33 -0300

    Add files via upload

---
+++ AAAAAAAAAAAAAAAA/apps.js
@@ -0,0 +1,25 @@
+//let artista1 = new Array("ricardo fort","feltfort")
+//let artista2 = new Array("lizy tagliani","telefe")
+//let artista3 = new Array("marley","telefe")
+
+//console.log(artista3)
+
+//let canciones= ["moscow mule","otro atrdecer", "party"]
+//console.log("canciones en la playlist " + canciones)
+
+//console.log(canciones.length)
+
+//let francia = new Array("Ratatouille","Brioche", "Cassoulet")
+//let españa = new Array("Tortilla de papas","Paella", "Jamón Ibérico")
+//let suiza = new Array("Fondue","Raclette", "Muesli de Bircher")
+
+let paises= ["Francia", "España", "suiza"]
+let comidas = ["Ratatouille", "Tortilla de papas", "Fondue"]
+let menu=[comidas, paises]
+console.log(menu [0][0])
+
+comidas.push("Tacos")
+comidas.pop("Fondue")
+
+comidas.unshift("empanadas")
+comidas.shift("empanadas")
